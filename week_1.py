5e1bf9ac7bd377ba7cb87a6c5390ad15c9eacec3
bchan42 <bchan3k@gmail.com>
2025-01-09 00:31:55 -0800

    check timestamp

--- week_1.py
+++ week_1.py
@@ -10,8 +10,8 @@ import csv
 with open("./2022_place_canvas_history.csv", "r") as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
-        print(row) # print out each row of csv
-
+        # print(row) # print out each row of csv
+        timestamp_str, id, color, location = row
 
 # example of a row
 # ['2022-04-04 01:04:50.028 UTC', 
@@ -21,8 +21,21 @@ with open("./2022_place_canvas_history.csv", "r") as file:
 
 # accept starting and ending hours from command line
 # check format: YYYY-MM-DD HH
+from datetime import datetime
+import sys
+
+def check_format(date_str):
+    try: 
+        return datetime.strftime(date_str, "%Y-%m-%d %H")
+    except ValueError:
+        sys.exit(1)
+
 # start hour before end hour
-# ...
+start_time = check_format(sys.argv[1])
+end_time = check_format(sys.argv[2])
+
+if start_time >= end_time:
+    sys.exit(1)
 
 
 # return most placed color during timeframe
