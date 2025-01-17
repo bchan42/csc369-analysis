a508b4ba3e1d94e0202fe17083cf86c3fad3ac78
bchan42 <bchan3k@gmail.com>
2025-01-17 10:29:42 -0800

    fixed memory issue in polars - lazyframe; finalized md's

--- week2/pandas.md
+++ week2/pandas.md
@@ -10,7 +10,7 @@
 - **Most Placed Color:** #000000
 - **Most Placed Pixel Location:** (0, 0)
 ## 6-Hour Timeframe
-- **Timeframe:** 2022-04-01 12 to 2022-04-01 15
-- **Execution Time:** 457047 ms
+- **Timeframe:** 2022-04-01 12 to 2022-04-01 18
+- **Execution Time:** 448623 ms
 - **Most Placed Color:** #000000
-- **Most Placed Pixel Location:** (0, 0)
+- **Most Placed Pixel Location:** (859, 766)
--- week2/polars.py
+++ week2/polars.py
@@ -2,53 +2,60 @@
 import polars as pl
 from datetime import datetime
 import sys
+from collections import Counter
 import time
 
+
 # process csv function
-def process_csv_pl(start_time, end_time):
+def process_csv(start_time, end_time):
 
-    # read in csv
-    df = pl.read_csv("./2022_place_canvas_history.csv")
+    color_count = Counter()
+    pixel_count = Counter()
 
-    # convert timestamp to datetime
-    df = df.with_columns(pl.col('timestamp').str.strptime(pl.Datetime, 
-                                                          fmt="%Y-%m-%d %H:%M:%S.%f UTC"))
+    batch_size = 100_000  # num rows to process per batch
+
+    # use polars' lazy frame (reduces memory usage)
+    # scan_csv filters row while reading instead of loading in entire dataset 
+    lazy_frame = pl.scan_csv("./2022_place_canvas_history.csv")
+
+    # iterate over slices after ..
+    for df_chunk in lazy_frame.with_columns(
+        pl.col("timestamp").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S%.f UTC", strict=False)
+    ).filter( #  filtering specific timestamp
+        (pl.col("timestamp") >= start_time) & (pl.col("timestamp") <= end_time) # starttime <= timestamp <= endtime
+    ).collect().iter_slices(batch_size): # data in batches reduces memory storage too
+
+        # increment num of colors & pixel locs
+        color_count.update(df_chunk["pixel_color"].to_list())
+        pixel_count.update(df_chunk["coordinate"].to_list())
+
+    # find most common color and location
+    most_comm_color = color_count.most_common(1)[0][0] if color_count else "No Color Data"
+    most_comm_pixel = pixel_count.most_common(1)[0][0] if pixel_count else "No Pixel Location Data"
     
-    # filter df with given timeframe
-    df_timeframe = df.filter((pl.col('timestamp') >= start_time) & (pl.col('timestamp') <= end_time))
-
-    # results (try something else) -- series mode?
-    most_comm_color = df_timeframe['color'].mode()[0] if len(df_timeframe['color'].mode()) > 0 else "No Color Data"
-    most_comm_pixel = df_timeframe['location'].mode()[0] if len(df_timeframe['location'].mode()) > 0 else "No Pixel Location Data"
-
     return most_comm_color, most_comm_pixel
 
 
 def main():
 
     try:
-        start_time = datetime.strptime(sys.argv[1], "%Y-%m-%d %H") # accept time in YYYY-MM-DD HH format
+        start_time = datetime.strptime(sys.argv[1], "%Y-%m-%d %H")
         end_time = datetime.strptime(sys.argv[2], "%Y-%m-%d %H")
 
         if start_time >= end_time:
             print("Error: End hour must be after the start hour.")
+            sys.exit(1)
 
     except ValueError:
         print("Error: Incorrect Date Format")
         sys.exit(1)
 
-    # start execution time
-    start = time.perf_counter_ns()
-    
-    # return most placed color during timeframe
-    # return most placed pixel location during that timeframe
-    most_comm_color, most_comm_pixel = process_csv_pl(start_time, end_time) # process data
+    start = time.perf_counter_ns() # start execution time
+    most_comm_color, most_comm_pixel = process_csv(start_time, end_time) # process data
+    end = time.perf_counter_ns() # end execution time
+    exec_time = (end - start) / 1_000_000 # convert ns to ms
 
-    # end execution time
-    end = time.perf_counter_ns()
-    exec_time = (end - start) / 1_000_000 # convert from ns to ms
-
-    # print result
+    # print results
     print(f"- **Timeframe:** {start_time.strftime('%Y-%m-%d %H')} to {end_time.strftime('%Y-%m-%d %H')}")
     print(f"- **Execution Time:** {exec_time:.0f} ms")
     print(f"- **Most Placed Color:** {most_comm_color}")
