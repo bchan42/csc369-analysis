711726c26101df4dc13c22dfd802e75f19cd6148
bchan42 <bchan3k@gmail.com>
2025-01-21 22:15:27 -0800

    add week 3 files

---
+++ week3/week_3.py
@@ -0,0 +1,9 @@
+# accept start & end hour as arguments
+# perform:
+    # rank colors by distinct users -> english
+    # calc avg session length
+    # pixel placement percentiles (50th, 75th, 90th, and 99th)
+    # count first time users
+
+# processed data < 3GB
+# calc tasks run < 30 sec
